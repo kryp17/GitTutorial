d2816f1f08531a7a2f4a13fe94a7d61c793e76e1
kryp17 <krupakhar978@gmail.com>
2020-04-03 11:38:06 +0530

    revert

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         print("Hello CS193P")
         print("hello world")
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
 
 
