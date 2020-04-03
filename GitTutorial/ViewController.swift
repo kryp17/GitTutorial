492e7751b648cbad104a0f1ed3652e8910cc0e3b
kryp17 <krupakhar978@gmail.com>
2020-04-03 11:33:47 +0530

    removed unnecessary comments

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         print("hello world")
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
