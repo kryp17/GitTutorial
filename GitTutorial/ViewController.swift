c86fc274639273fdb8f7049ad64cce9fa49233cc
kryp17 <krupakhar978@gmail.com>
2020-04-03 11:27:42 +0530

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by krupakhar gandeepan on 03/04/20.
+//  Copyright © 2020 krupakhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
